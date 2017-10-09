a6baf1de86b8ad9bfd3510b628ef3f4dd688b182
Oleg Grigorov <golanster@gmail.com>
2017-10-09 13:16:30 +0300

    adding kvm type

--- test-openstack-component-pipeline.groovy
+++ test-openstack-component-pipeline.groovy
@@ -46,7 +46,7 @@ node("${SLAVE_NODE}") {
     def project = PROJECT
     def pkgReviewNameSpace
     def extra_repo = EXTRA_REPO
-    def testrail = true
+    def testrail = false
     def test_milestone = ''
     def test_tempest_pattern = TEST_TEMPEST_PATTERN
     def stack_deploy_job = "deploy-${STACK_TYPE}-${TEST_MODEL}"
